e60a0e5cfa9b15d974d4dd713d21f58efe472323
evelinanorlin <evelina@sphinxly.se>
2024-01-25 10:48:42 +0100

    Merged

--- client/src/Leaflet/osm-providers.tsx
+++
@@ -1,9 +0,0 @@
-export default {
-  maptiler: {
-    url: `https://api.maptiler.com/maps/outdoor-v2/256/{z}/{x}/{y}.png?key=${
-      import.meta.env.VITE_MAPTILER_KEY
-    }`,
-    attribution:
-      "<a href='https://www.maptiler.com/copyright/' target='_blank' style='position:absolute;left:10px;bottom:10px;z-index:999;'>&copy; MapTiler</a><a href='https://www.openstreetmap.org/copyright' target='_blank'>&copy; OpenStreetMap contributors</a>",
-  },
-};
--- client/src/functions/storage.ts
+++
@@ -1,3 +0,0 @@
-export function setIsVisited(): void {
-  localStorage.setItem("isVisited", "true");
-}
